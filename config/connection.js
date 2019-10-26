7f872d1c40ed3cfee0e0dce60c63f901ce51a818
Chris Flick <chrisflick@gmail.com>
2019-10-26 15:23:56 -0700

    modified connection in config to use heroku database

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,21 @@ var mysql = require("mysql");
 
 const password = require('./secrets.js')
 
+
 var connection;
 
 
-if (proccess.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
+// if (proccess.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
  connection = mysql.createConnection({
-    host: "localhost",
+    host: "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: password,
-    database: "burgers_db"
+    user: "svqwzklz3gfg7q90",
+    password: "degg28cldf8hcxaj",
+    database: "dw3wfuulx3cjnlqq"
   });
-}
+// }
 
 
 connection.connect(function(err) {
