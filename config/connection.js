96793bedff3edeb79460059e2ec903af53db99ac
Chris Flick <chrisflick@gmail.com>
2019-10-26 15:37:17 -0700

    removed secrets

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 
-const password = require('./secrets.js')
+// const password = require('./secrets.js')
 
 
 var connection;
@@ -9,17 +9,17 @@ var connection;
 // if (proccess.env.JAWSDB_URL) {
 //   connection = mysql.createConnection(process.env.JAWSDB_URL)
 // } else {
- connection = mysql.createConnection({
-    host: "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "svqwzklz3gfg7q90",
-    password: "degg28cldf8hcxaj",
-    database: "dw3wfuulx3cjnlqq"
-  });
+connection = mysql.createConnection({
+  host: "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "svqwzklz3gfg7q90",
+  password: "degg28cldf8hcxaj",
+  database: "dw3wfuulx3cjnlqq"
+});
 // }
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
