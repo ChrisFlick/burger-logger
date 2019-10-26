3052f3a1b4b51c9f19e7a98e5c837a01360e23f9
Chris Flick <chrisflick@gmail.com>
2019-10-26 12:33:08 -0700

    index.handlebars now correctly displays eaten and non eaten burgers

--- routes/routes.js
+++ routes/routes.js
@@ -3,15 +3,6 @@ const orm = require('../config/orm')
 module.exports = function (app) {
     app.get("/", function (req, res) {
         orm.selectAll().then(burgers => {
-           
-            
-            for (let i = 0; i < burgers.length; i++) {
-                if (burgers[i].devoured === 0) {
-                    burgers[i].eaten = false
-                } else {
-                    burgers[i].eaten = true;
-                }
-            }
 
             console.log(burgers)
 
